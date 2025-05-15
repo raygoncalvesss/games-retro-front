9837e79cf1b55cf87a79243547a910738b9d7ab2
Rayssa <158166558+raygoncalvesss@users.noreply.github.com>
2025-05-15 09:36:22 -0300

    feat: create search input

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,8 +4,8 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Games Retro</h1>
-      <p>Recordes de jogos retro</p>
+      <h1>Games Retrô</h1>
+      <p>Recordes de jogos retrô</p>
     </main>
   );
 }
